bf09e12dcb11e325e5b4f7a671fba86ccc8912c0
Omer Faruk Buyukbas <68295684+omerbykbs@users.noreply.github.com>
2024-01-04 18:47:04 +0100

    Update appointment.py

--- Checking For Appointment/appointment.py
+++ Checking For Appointment/appointment.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import smtplib
 import getpass
 
-website = # write here the website in that you want to get an appointment
+# write here the website in that you want to get an appointment
+website = "https://www.service.bremen.de/dienstleistungen/personalausweis-beantragen-8363"
 
 response = requests.get(website)
 
@@ -13,7 +14,7 @@ smtp_object.ehlo()
 
 smtp_object.starttls()
 
-# Write your own E-mail to send message and use here your App Password that you can get from your Gmail account.
+# Write your own E-mail address to send message and use here your App Password that you can get from your Gmail account.
 email = getpass.getpass("Email: ")
 password = getpass.getpass("Password: ")
 smtp_object.login(email,password)
@@ -24,51 +25,55 @@ subject = "New Appointment"
 message = "Be fast and get your appointment in"
 msg = "Subject : "+subject+'\n'+message+'\n'+website+"!"
 
+# Schedule the task to run every 5 minutes
+schedule.every(5).minutes.do(sendMail)
 
-if response.status_code == 200:
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.text, 'html.parser')
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
-    # Looking for a div element with a specific class
-    specific_div = soup.find('div', class_='fix_ul')
+def sendMail():
     
-    # Check if the div element was found
-    if specific_div:
-        # Find ul element with a specific class within the div
-        ul_element = soup.find('ul',class_='list')
-
-        # Check if the ul element was found
-        if ul_element:
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.text, 'html.parser')
+    
+        specific_div = soup.find('div', class_='fix_ul')
+        
+        isAppointment = 0
+        
+        # Check if the div element was found
+        if specific_div:
             # Find all li elements within the ul
-            li_elements = ul_element.find_all('li')
-
-            # Loop through the li elements and extract information
-            for li in li_elements:
-                # Extract the text content of the anchor tag within the li
-                anchor_text = li.find('a', class_='more').text
+            ul_element = soup.find('ul',class_='list')
     
-                # Extract additional information (if present)
-                additional_info = li.find('a', class_='direct')
-                if additional_info:
-                    additional_info_text = additional_info.text
-                else:
-                    additional_info_text = None
+            if ul_element:
+                # Find all li elements within the ul
+                li_elements = ul_element.find_all('li')
     
-                # Print the extracted information
-                print(f"Text: {anchor_text}")
-                print(f"Additional Info: {additional_info_text}")
-                print("------")
+                # Loop through the li elements and extract information
+                for li in li_elements:
+                    # Extract the text content of the anchor tag within the li
+                    anchor_text = li.find('a', class_='more').text
+        
+                    # Extract additional information (if present)
+                    additional_info = li.find('a', class_='direct')
+                    if additional_info:
+                        additional_info_text = additional_info.text
+                    else:
+                        additional_info_text = None
+                    
+                    if additional_info_text == "Termin buchen":
+                        isAppointment += 1 
 
-                # Check if there is an available appointment
-                if additional_info_text == "Termin buchen":
-                    # Send Email and inform to get that appointment
+                if isAppointment > 0:
                     smtp_object.sendmail(from_address,to_address,msg)
-
+                else:
+                    print("There is no available appointment :(")
+            else:
+                print('Ul element with class "list" not found.')
     
         else:
-            print('Ul element with class "list" not found.')
-
+            print('Div element with class "your-class-name" not found.')
     else:
-        print('Div element with class "your-class-name" not found.')
-else:
-    print('Failed to retrieve the webpage. Status code:', response.status_code)
+        print('Failed to retrieve the webpage. Status code:', response.status_code)
